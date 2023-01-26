f5dab134447a3289c65d784f8b302d152bc0f580
Murchey <98136625+Murchey@users.noreply.github.com>
2023-01-26 20:40:20 +0800

    Add files via upload

---
+++ SysTools/View/SysTestForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace SysTools
+{
+    partial class SysTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SysTestForm));
+            this.lbItems = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbItems
+            // 
+            this.lbItems.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.lbItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbItems.Font = new System.Drawing.Font("黑体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbItems.ForeColor = System.Drawing.Color.White;
+            this.lbItems.FormattingEnabled = true;
+            this.lbItems.ItemHeight = 23;
+            this.lbItems.Location = new System.Drawing.Point(0, 0);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.Size = new System.Drawing.Size(758, 452);
+            this.lbItems.TabIndex = 0;
+            // 
+            // SysTestForm
+            // 
+            this.ClientSize = new System.Drawing.Size(758, 452);
+            this.Controls.Add(this.lbItems);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SysTestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "System Informations";
+            this.Load += new System.EventHandler(this.SysTestForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbItems;
+    }
+}
